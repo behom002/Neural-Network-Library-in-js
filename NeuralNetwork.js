9fd28eadb81b20a16ae189812098ee9887d4a2d3
behom002 <benedict.homuth@gmail.com>
2018-12-23 13:30:46 -0800

    Basic neural network class

---
+++ NeuralNetwork.js
@@ -0,0 +1,12 @@
+class NeuralNetwork{
+
+  //This will only support neural networks with 3 layers
+  //Creates a neural network witch the given number of nodes
+  constructor(numberInput, numberHidden, numberOutput){
+  this.input_nodes = numberInput;
+  this.hidden_nodes = numberHidden;
+  this.output_nodes = numberOutput;
+  }
+
+
+}
